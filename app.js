3f853e37c582f6ee1f045f8c230a01ae11f45ce1
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-07-17 00:39:17 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getAuth , createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBzU3W1aY0lPl6vd9jzxBmM8gKVMVHcTsc",
   authDomain: "file-store-9ebe4.firebaseapp.com",
@@ -11,41 +13,57 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 
 
 let register = document.getElementById("register_btn");
 
-register.addEventListener("click", ()=>{
-    let name =document.getElementById("name");
-    let phone =document.getElementById("phone");
-    let email =document.getElementById("email");
-    let password =document.getElementById("password");
+register.addEventListener("click", () => {
+  let name = document.getElementById("name");
+  let phone = document.getElementById("phone");
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
 
 
-    let user = {
-        name : name.value,
-        phone : phone.value,
-        email : email.value,
-        password : password.value
-    }
+  let userData = {
+    name: name.value,
+    phone: phone.value,
+    email: email.value,
+    password: password.value
+  }
 
 
-    createUserWithEmailAndPassword(auth, user.email, user.password)
-  .then((userCredential) => {
-     const user = userCredential.user;
-     console.log(user, "user")
-    
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage , "errorMessage")
-    
-  });
+  createUserWithEmailAndPassword(auth, userData.email, userData.password)
+    .then(async (userCredential) => {
+      const user = userCredential.user;
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+          ...userData,
+          uid: user.uid
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage, "errorMessage")
+
+    });
 })
 
 
-function abc(){
+let getAllUsers = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} =>` , doc.data());
+  });
 
 }
+getAllUsers()
+
+
